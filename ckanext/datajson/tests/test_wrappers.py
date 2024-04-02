2b24fb308523a6953b449566c4777d484edeaf2a
Jay Guo <jguo144@gmail.com>
2024-04-02 14:40:42 -0400

    Add tests for get_license wrapper

--- ckanext/datajson/tests/test_wrappers.py
+++ ckanext/datajson/tests/test_wrappers.py
@@ -85,3 +85,49 @@ class TestCatalogDateWrapper(object):
         }
         modified_date = Wrappers.get_catalog_date(Wrappers.pkg.get('metadata_modified'))
         assert modified_date == "2021-03-26T00:45:51.542439"
+
+
+class TestGetLicenseWrapper(object):
+
+    def test_valid_license_url(self):
+        Wrappers.pkg = {
+            "title": "Test Dataset",
+            "name": "test-dataset",
+            "license_id": "cc-by",
+            "license_title": "Creative Commons Attribution",
+            "license_url": "http://www.opendefinition.org/licenses/cc-by"
+        }
+        Wrappers.current_field_map = {
+            "field": "license_url",
+            "wrapper": "get_license"
+        }
+        license = Wrappers.get_license(Wrappers.pkg.get('license_url'))
+        assert license == "http://www.opendefinition.org/licenses/cc-by"
+
+    def test_no_license_url(self):
+        Wrappers.pkg = {
+            "title": "Test Dataset",
+            "name": "test-dataset",
+            "license_id": "other-open",
+            "license_title": "Other (Open)"
+        }
+        Wrappers.current_field_map = {
+            "field": "license_url",
+            "wrapper": "get_license"
+        }
+        license = Wrappers.get_license(Wrappers.pkg.get('license_url'))
+        assert license == "Other (Open)"
+
+    def test_no_license_url_and_no_license_title(self):
+        Wrappers.pkg = {
+            "title": "Test Dataset",
+            "name": "test-dataset",
+            "license_id": None,
+            "license_title": None
+        }
+        Wrappers.current_field_map = {
+            "field": "license_url",
+            "wrapper": "get_license"
+        }
+        license = Wrappers.get_license(Wrappers.pkg.get('license_url'))
+        assert license is None
